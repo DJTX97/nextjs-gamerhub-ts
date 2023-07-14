d4376d53e0fab8b107928032f49e5621df2bda63
DJTX97 <the.10.of.one@gmail.com>
2023-07-14 16:46:10 +0300

    small 2

--- app/components/UtilityComp/GameLoader.tsx
+++ app/components/UtilityComp/GameLoader.tsx
@@ -1,6 +1,6 @@
 export default function GameLoader() {
   return (
-    <div className="h-[1000px] 2xl:h-[1500px] w-full mx-10 flex flex-col items-center bg-theme_black rounded-2xl">
+    <div className="h-[1000px] 2xl:h-[1500px] w-full mx-10 flex flex-col items-center rounded-2xl">
       <div
         className="mt-56 animate-spin inline-block w-40 h-40 border-[20px] border-current border-t-transparent text-theme_medium_violet rounded-full"
         role="status"
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation";
 
 export default function NotFound() {
  
-  return <>{redirect("/")}</>;
+  return <div className="h-[1000px] 2xl:h-[1500px] w-full mx-10 flex flex-col items-center rounded-2xl">{redirect("/")}</div>;
 }

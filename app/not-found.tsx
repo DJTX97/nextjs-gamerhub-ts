86248b0eb116762999f9c68e488323a835c39502
DJTX97 <the.10.of.one@gmail.com>
2023-07-14 16:49:10 +0300

    small 3

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,7 @@
-import { redirect } from "next/navigation";
+'use client'
+import { useRouter } from "next/navigation";
 
 export default function NotFound() {
- 
-  return <div className="h-[1000px] 2xl:h-[1500px] w-full mx-10 flex flex-col items-center rounded-2xl">{redirect("/")}</div>;
+  const router = useRouter()
+  router.push("/");
 }

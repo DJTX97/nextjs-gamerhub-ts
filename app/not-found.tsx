3143ea01f14ab529e5ee70c160e2b312d9cf8d03
DJTX97 <the.10.of.one@gmail.com>
2023-07-15 15:13:15 +0300

    minor improvement

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,34 +1,13 @@
-"use client"
+"use client";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import HomeTitle from "./components/ContentComp/HomeTitle";
+import HomeLoader from "./components/UtilityComp/HomeLoader";
 
 export default function NotFound() {
-  const router = useRouter()
+  const router = useRouter();
   useEffect(() => {
-    router.replace("/")
-  }, [])
-  
+    router.replace("/");
+  }, []);
 
-  return (
-    <div className="w-full mx-10 bg-theme_black rounded-2xl">
-      <HomeTitle />
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-14 md:gap-y-10 2xl:gap-28 px-10 pb-3 md:pb-5 pt-10 md:pt-12 2xl:px-20">
-        {Array.from({ length: 6 }, (_, i) => (
-          <div
-            className="flex flex-col justify-center h-72 2xl:h-80 cursor-pointer hover:opacity-80 hover:scale-105 transition-all duration-[150ms]"
-            key={i}
-          >
-            <div className="h-1/2 w-full bg-theme_light_gray rounded-t-xl animate-pulse" />
-            <div className="h-1/2 w-full flex flex-col align-middle gap-2 px-2 py-1 sm:py-2 bg-theme_dark_gray rounded-b-xl text-[15px]">
-              <div className="h-8 mt-1 bg-theme_light_gray rounded-full animate-pulse"/>
-              <div className="h-4 w-1/2 bg-theme_light_gray rounded-full animate-pulse"/>
-              <div className="h-4 w-1/2 bg-theme_light_gray rounded-full animate-pulse"/>
-              <div className="h-4 w-1/2 bg-theme_light_gray rounded-full animate-pulse"/>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
+  return <HomeLoader />;
 }

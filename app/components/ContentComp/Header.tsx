d49e8c84e27f81350b087fe52b1e910c6938048a
DJTX97 <the.10.of.one@gmail.com>
2023-09-24 12:05:50 +0300

    feat: changed site name

--- app/components/ContentComp/Header.tsx
+++ app/components/ContentComp/Header.tsx
@@ -9,7 +9,7 @@ const games = await fetchData(url);
 export default function Header() {
   return (
     <div className="flex justify-between items-center px-4 md:px-8 py-3 2xl:py-6 bg-theme_black text-3xl sm:text-4xl 2xl:text-6xl font-bold border-b-4 border-theme rounded-b-2xl">
-      <Link href={"/"}>GamerHub</Link>
+      <Link href={"/"}>GamerzHub</Link>
       <SearchBar games={games} />
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import Header from "./components/ContentComp/Header";
 import Footer from "./components/ContentComp/Footer";
 
 export const metadata = {
-  title: "GamerHub",
+  title: "GamerzHub",
   description: "A site for true gamers.",
   icons: "../../assets/favicon/fav.png",
 };
